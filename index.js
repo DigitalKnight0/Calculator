ba7ef960173b839a52cbf9d8b90809089975caa5
DigitalKnight0 <asadredset@gmail.com>
2022-06-22 22:22:39 +0500

    Add keyboard Support

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const numbers = document.querySelectorAll('.number')
 const operators = document.querySelectorAll('.operate')
-const display = document.querySelector('.display-bar')
+const display = document.querySelector('.main-display')
+const sideDisplay = document.querySelector('.side-display')
 const evalButton = document.querySelector('.result')
+const clrButton = document.querySelector('.clear')
+const delButton = document.querySelector('.delete')
 const cache = []
 const numberStore = []
 let currentOperator = ''
 let operatorPressed = false
+
+document.addEventListener('keydown', handleKey)
+
+function handleKey(e){
+    let currentKey = e.key
+    if(!Number.isNaN(Number(currentKey))){
+        handleNumbers(e)
+    } else if(['+', '-', '*', '/', '%', '^'].includes(currentKey)){
+        handleOperators(e)
+    } else if(currentKey === 'Enter' || currentKey === '='){
+        handleEval()
+    } else if(currentKey === 'Escape'){
+        handleClr()
+    } else if(currentKey === 'Backspace' || currentKey === 'Delete'){
+        handleDel()
+    }
+}
 
 numbers.forEach(number => {
     number.addEventListener('click', handleNumbers)
@@ -15,27 +35,64 @@ operators.forEach(operator => {
     operator.addEventListener('click', handleOperators)
 })
 
+evalButton.addEventListener('click', handleEval)
+
+clrButton.addEventListener('click', handleClr)
+
+function handleClr(){
+    currentOperator = ''
+    operatorPressed = false
+    cache.length = 0
+    numberStore.length = 0
+    updateDisplay('', true)
+    sideDisplay.textContent = ''
+}
+
+delButton.addEventListener('click', handleDel)
+
+function handleDel(){
+    let currentDisplay = display.textContent.split('')
+    currentDisplay.pop()
+    display.textContent = currentDisplay.join('')
+    numberStore.pop()
+}
+
+function handleEval(){
+    if(!cache.length || !numberStore.length) return
+    let result = evaluate()
+    updateCache(result)
+    updateDisplay(result, true)
+    updateSideDisplay()
+    operatorPressed = true
+}
+
 function handleNumbers(e){
-    let currentNumber = e.target.textContent
+    let currentNumber = e.type === "keydown" ? e.key : e.target.textContent
+    if(currentNumber === '.' && numberStore.includes('.')){
+        return
+    }
     updateDisplay(currentNumber, false)
     numberStore.push(currentNumber)
 }
 
 function handleOperators(e){
+    let operator = e.type === "keydown" ? e.key : e.target.textContent
     if(!cache.length && !numberStore.length){
         return
     } else if(!cache.length && numberStore.length){
         updateCache(false)
-        currentOperator = e.target.textContent
+        currentOperator = operator
         operatorPressed = true
     } else if(cache.length && !numberStore.length){
-        currentOperator = e.target.textContent
+        currentOperator = operator
+        updateSideDisplay()
     } else {
         let result = evaluate()
         updateCache(result)
         updateDisplay(result, true)
         operatorPressed = true
-        currentOperator = e.target.textContent
+        currentOperator = operator
+        updateSideDisplay()
     }
 }
 
@@ -45,6 +102,11 @@ function updateDisplay(currentNumber, clrscr){
         operatorPressed = false
     }
     display.textContent += currentNumber
+}
+
+function updateSideDisplay(){
+    let content = `${cache[0]} ${currentOperator}`
+    sideDisplay.textContent = content
 }
 
 function updateCache(option){
